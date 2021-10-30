917ee5b89d90722f83b02f22f3d487904466c5fa
walterYangana <medinawalter3@gmail.com>
2021-10-30 11:55:55 -0500

    eliminando archivos que no se necesitan

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Fragment } from 'react/cjs/react.development';
+import Ejemplo from './components/Ejemplo';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Hola mundo</h1>
+      <Ejemplo/>
+
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Ejemplo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//sfc
+
+const Ejemplo = () => {
+    return (
+        <h3>Mi primer componente</h3>
+      );
+}
+ 
+export default Ejemplo;
+
